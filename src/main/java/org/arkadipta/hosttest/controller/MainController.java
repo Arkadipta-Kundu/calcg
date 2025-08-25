465f381c2c79b7fc3aaf75d571ecbadbc1cd64be
Arkadipta-Kundu <arkadiptak49@gmail.com>
2025-08-25 10:36:17 +0530

    Add CORS support for the calculator API

--- src/main/java/org/arkadipta/hosttest/controller/MainController.java
+++ src/main/java/org/arkadipta/hosttest/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/calculator")
 public class MainController {
